4b9c17736ac6046b0bb18cb8403ccdb9008b684c
Guilherme Webber <guiz1n.webber@gmail.com>
2023-04-22 17:07:06 -0300

    finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,13 +29,50 @@ dropButton.addEventListener("mouseover", () => {
 //mobile
 let imagemMenu = document.getElementById("imagemMenu");
 let imagemMenuClose = document.getElementById("imagemMenuClose");
+let menuOpen = document.getElementById("menuOpen");
+let main = document.getElementById("main");
 
 imagemMenu.addEventListener("click", () => {
-  imagemMenuClose.style.display = 'block';
-  imagemMenu.style.display = 'none'
+  imagemMenuClose.style.display = "block";
+  imagemMenu.style.display = "none";
+  menuOpen.style.display = "block";
+  let backdrop = document.getElementById("backdrop");
+  backdrop.style.display = "block";
 });
 
-imagemMenuClose.addEventListener('click', () => {
-  imagemMenu.style.display = 'block'
-  imagemMenuClose.style.display = 'none';
-})
+imagemMenuClose.addEventListener("click", () => {
+  imagemMenu.style.display = "block";
+  imagemMenuClose.style.display = "none";
+  menuOpen.style.display = "none";
+  backdrop.style.display = "none";
+});
+
+//first drop mobile
+let firstDropMobile = document.getElementById("firstDropMobile");
+firstDropMobile.textContent = "Features ▼";
+let myDropMobile = document.getElementById("myDropMobile");
+myDropMobile.style.display = "none";
+firstDropMobile.addEventListener("click", () => {
+  if (myDropMobile.style.display === "block") {
+    myDropMobile.style.display = "none";
+    firstDropMobile.textContent = "Features ▼";
+  } else {
+    firstDropMobile.textContent = "Features ▲";
+    myDropMobile.style.display = "block";
+  }
+});
+
+let secondDropMobile = document.getElementById("secondDropMobile");
+secondDropMobile.textContent = "Company ▼";
+let menuDropMobile = document.getElementById("menuDropMobile");
+menuDropMobile.style.display = "none";
+
+secondDropMobile.addEventListener("click", () => {
+  if (menuDropMobile.style.display === 'block') {
+    menuDropMobile.style.display = 'none';
+    secondDropMobile.textContent = "Company ▼";
+  } else {
+    menuDropMobile.style.display = "block";
+    secondDropMobile.textContent = "Company ▲";
+  }
+});
